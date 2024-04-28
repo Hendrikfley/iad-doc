8b20b87cba11982d1d915d51c7641bb4b4b5d5fc
Hendrik Fley <hendrik.fley@web.de>
2024-04-28 18:35:02 +0200

    built via npm correctly (hopefully)

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-      publicPath: process.env.NODE_ENV === "production" ? "/iad-doc//" : "/",
+      publicPath: process.env.NODE_ENV === "production" ? "/iad-doc/" : "/",
 },
     )
 
